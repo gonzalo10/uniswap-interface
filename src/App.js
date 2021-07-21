70e8730b7afd7839368d56e24ff5514b98cb2d9f
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-07-21 22:39:10 +0200

    feat: load all uniswap tokens

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import { useState } from 'react'
 import Web3 from 'web3'
 import { JsonRpcProvider, Web3Provider } from '@ethersproject/providers'
 import BurnerProvider from 'burner-provider'
+import {
+	ChainId,
+	Token,
+	WETH,
+	Fetcher,
+	Trade,
+	TokenAmount,
+	Percent
+} from '@uniswap/sdk'
 
 import './App.css'
 import { useEffect } from 'react'
@@ -12,11 +21,43 @@ function getProvider(localProvider) {
 	burnerConfig.rpcUrl = localProvider.connection.url
 	return new Web3Provider(new BurnerProvider(burnerConfig))
 }
+function tokenListToObject(array) {
+	return array.reduce((obj, item) => {
+		obj[item.symbol] = new Token(
+			item.chainId,
+			item.address,
+			item.decimals,
+			item.symbol,
+			item.name
+		)
+		return obj
+	}, {})
+}
+const getTokenList = async (tokenListURI) => {
+	try {
+		let tokenList = await fetch(tokenListURI)
+		let tokenListJson = await tokenList.json()
+		let filteredTokens = tokenListJson.tokens.filter(function (t) {
+			return t.chainId === ChainId.MAINNET
+		})
+		let ethToken = WETH[ChainId.MAINNET]
+		ethToken.name = 'Ethereum'
+		ethToken.symbol = 'ETH'
+		ethToken.logoURI =
+			'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png'
+		let _tokenList = [ethToken, ...filteredTokens]
+		return _tokenList
+	} catch (e) {
+		console.log(e)
+	}
+}
 
 function App() {
 	const [tokenListURI, setTokenListURI] = useState(
 		'https://gateway.ipfs.io/ipns/tokens.uniswap.org'
 	)
+	const [tokenList, setTokenList] = useState()
+	const [tokens, setTokens] = useState()
 
 	const localProviderUrl = 'http://localhost:8545'
 	const localProvider = new JsonRpcProvider(localProviderUrl)
@@ -30,7 +71,12 @@ function App() {
 		const etherBalance = formatEther(balance)
 		const parsedBalance = parseFloat(etherBalance)
 		console.log(accounts[0], parsedBalance)
+		const _tokenList = await getTokenList(tokenListURI)
+		setTokenList(_tokenList)
+		setTokens(tokenListToObject(_tokenList))
 	}
+
+	console.log({ tokens })
 	useEffect(() => {
 		loadBlockchainData()
 	}, [])
